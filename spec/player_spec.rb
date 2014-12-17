faf76c2becada4a2a4fe7ddd487eaca65c43323c
Elena <elena.15@hotmail.it>
2014-12-17 10:40:26 +0000

    player can choose a weapon in the logic of the game.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,19 @@ require 'player'
 
 describe Player do
 
+  let(:player) {Player.new('Elena')}
+
   it "should have a name" do
-    player = Player.new('Elena')
     expect(player.name).to eq('Elena')
   end
 
   it "should not have a weapon wen initialized" do
-    player = Player.new('Elena')
     expect(player.weapon).to eq(nil)
+  end
+
+  it "should be able to choose a weapon" do
+    player.weapon = "ROCK"
+    expect(player.weapon).to eq("ROCK")
   end
 
 end
