d0affec1b10a3cee43647631df88b655d0f6ea86
Elena <elena.15@hotmail.it>
2014-12-17 10:46:52 +0000

    the player of the game has a name

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,16 @@ require 'game'
 
 describe Game do
 
-  let(:game) {Game.new}
+  let(:game)   {Game.new}
+  let(:player) {Player.new('Elena')}
 
   it "should be initialized with a player" do
     expect(game.player).to eq(nil)
+  end
+
+  it "should be able to add a player and his/her name" do
+    game.player = player
+    expect(game.player.name).to eq('Elena')
   end
 
 end
